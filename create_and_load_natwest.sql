e4b0573afb19576a0931f930110e1bcfacf0243d
anjaanmd <mohandoss.ez@gmail.com>
2016-11-03 04:58:07 +0530

    added 'optionally enclosed by quote' in load data infile procedure

--- create_and_load_natwest.sql
+++ create_and_load_natwest.sql
@@ -74,12 +74,12 @@ CREATE TABLE natwest_bowling(
 
 use natwest_2015;
 
-LOAD DATA INFILE '/var/lib/mysql-files/2015_Eng_Natwest_t20_blast.match_details.csv' INTO TABLE  natwest_match_details FIELDS TERMINATED BY ','   LINES TERMINATED BY '\n' IGNORE 1 LINES;
+LOAD DATA INFILE '/var/lib/mysql-files/2015_Eng_Natwest_t20_blast.match_details.csv' INTO TABLE  natwest_match_details FIELDS TERMINATED  BY ',' OPTIONALLY ENCLOSED BY '"'    LINES TERMINATED BY '\n' IGNORE 1 LINES;
 
-LOAD DATA INFILE '/var/lib/mysql-files/2015_Eng_Natwest_t20_blast.batting.csv' INTO TABLE  natwest_batting FIELDS TERMINATED BY ','   LINES TERMINATED BY '\n' IGNORE 1 LINES;
+LOAD DATA INFILE '/var/lib/mysql-files/2015_Eng_Natwest_t20_blast.batting.csv' INTO TABLE  natwest_batting FIELDS TERMINATED  BY ',' OPTIONALLY ENCLOSED BY '"'    LINES TERMINATED BY '\n' IGNORE 1 LINES;
 
 
-LOAD DATA INFILE '/var/lib/mysql-files/2015_Eng_Natwest_t20_blast.bowling.csv' INTO TABLE  natwest_bowling FIELDS TERMINATED BY ','   LINES TERMINATED BY '\n' IGNORE 1 LINES;
+LOAD DATA INFILE '/var/lib/mysql-files/2015_Eng_Natwest_t20_blast.bowling.csv' INTO TABLE  natwest_bowling FIELDS TERMINATED  BY ',' OPTIONALLY ENCLOSED BY '"'    LINES TERMINATED BY '\n' IGNORE 1 LINES;
 
-LOAD DATA INFILE '/var/lib/mysql-files/2015_Eng_Natwest_t20_blast.score_details.csv' INTO TABLE  natwest_score_details FIELDS TERMINATED BY ','   LINES TERMINATED BY '\n' IGNORE 1 LINES;
+LOAD DATA INFILE '/var/lib/mysql-files/2015_Eng_Natwest_t20_blast.score_details.csv' INTO TABLE  natwest_score_details FIELDS TERMINATED  BY ',' OPTIONALLY ENCLOSED BY '"'    LINES TERMINATED BY '\n' IGNORE 1 LINES;
 
